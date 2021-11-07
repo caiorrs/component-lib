2166bc484f92a5ecfe2eb53d05bede03b27496f0
caiorrs <caiorrs@gmail.com>
2021-11-06 21:22:40 -0300

    Added test component

---
+++ example/storybook/stories/TestComponent/TestComponent.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { TestComponent } from '../../../../src/components';
+import { storiesOf } from '@storybook/react-native';
+
+storiesOf('TestComponent', module).add('basic test', () => <TestComponent />);
---
+++ src/components/TestComponent/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+const TestComponent = () => {
+  return (
+    <View>
+      <Text>This is the test component</Text>
+    </View>
+  );
+};
+
+export default TestComponent;
